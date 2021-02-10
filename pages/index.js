e963f5a1d8c2b0d81d391832b43404ad03923207
cpnunez <78775156+cpnunez@users.noreply.github.com>
2021-02-09 23:16:21 -0300

    Primeira Página

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import {useState} from 'react'
+export default Home
+
+function Home() {
+    return (
+        <div>
+        <h2>Home</h2>
+        <Contador/>
+        </div>
+
+    )
+}
+
+function Contador() {
+    const [contador, setContador] = useState(1);
+
+    function adicionarContador(){
+        setContador(contador + 1);
+
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+
+}
+
